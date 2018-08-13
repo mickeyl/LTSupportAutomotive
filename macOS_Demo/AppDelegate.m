f51b962421f211ee6af5c733f79190117d7cac5e
Dr. Michael Lauer <mickey@vanille-media.de>
2018-08-13 14:13:59 +0200

    demo: add a macOS demo target (WIP)

---
+++ macOS_Demo/AppDelegate.m
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.m
+//  macOS_Demo
+//
+//  Created by Dr. Michael Lauer on 13.08.18.
+//  Copyright © 2018 Dr. Lauer Information Technology. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    // Insert code here to initialize your application
+}
+
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+    // Insert code here to tear down your application
+}
+
+
+@end
